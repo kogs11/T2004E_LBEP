15f002556503b617d4483a3b7c56239286e68879
Dinh Lap <63952696+kogs11@users.noreply.github.com>
2020-05-17 10:29:26 +0700

    Update lab6_3.cpp

--- lab6/lab6_3.cpp
+++ lab6/lab6_3.cpp
@@ -1,31 +1,27 @@
 #include <stdio.h>
 
-int main(){
-	int n,dem=0;
-	int ary[n];
-	
-	do{
-		printf("Nhap vao gia tri n: ");
-		scanf("%d",&n);
-	}while(n<=0);
-	
+int main (){
+	int n;
+	printf("Nhap n=");
+	scanf("%d",&n);
+	int ary[n],dem;
 	for(int i=1;i<=n;i++){
-		printf("Nhap vao so thu %d: ",i);
+		printf("Nhap so thu %d: ",i);
 		scanf("%d",&ary[i]);
 	}
 	
 	int x;
-	printf("Nhap vao so: ");
-	scanf("%d",x);
+	printf("Nhap x=");
+	scanf("%d",&x);
 	
 	for(int i=1;i<=n;i++){
-		if(x == ary[i]){
-			printf("X thuoc mang");
+		if(ary[i]==x){
+			printf("x thuoc mang");
 			dem++;
 			break;
 		}
 	}
 	if(dem==0){
-		printf("X ko thuoc mang");
+		printf("x ko thuoc mang");
 	}
 }
