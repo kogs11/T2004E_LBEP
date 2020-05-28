d4f6dff0bd53ba67da3cfc9ff78ae5cdc0f77525
Dinh Lap <63952696+kogs11@users.noreply.github.com>
2020-05-28 17:38:22 +0700

    Create assignment9_1_1.cpp

---
+++ assignment 9/assignment9_1_1.cpp
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <string.h>
+
+int main(){
+	// Nhap mang n chuoi
+	int n;
+	do{
+		printf("Nhap so nguyen duong n: ");
+		scanf("%d",&n);
+	}while (n<=0);
+	
+	char ary[n][100];
+	for(int i=0;i<n;i++){
+		printf("Nhap chuoi thu %d",i);
+		gets(ary[i]);
+	}
+	
+	// Nhap chuoi s
+	char s[100];
+	printf("Nhap chuoi s: ");
+	gets(s);
+	
+	// Kiem tra
+	int count=0;
+	for(int i=0;i<n;i++){
+		if(strcmp(s,ary[i])==0){
+			printf("chuoi s thuoc mang");
+			break;
+		}else count++;
+	}
+	if(count!=0) printf(" chuoi s khong thuoc mang");
+}
