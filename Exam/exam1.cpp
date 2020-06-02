fe7a48781e015a9b7e13fb234859aba79f63c3b1
Dinh Lap <63952696+kogs11@users.noreply.github.com>
2020-06-02 19:56:11 +0700

    update

--- Exam/exam1.cpp
+++ Exam/exam1.cpp
@@ -1,3 +1,20 @@
 #include <stdio.h>
+#include <string.h>
 
+void concat(char s1[], char s2[]){
+	strcat(s1," ");
+	strcat(s1,s2);
+}
 
+int main(){
+	char s1[100], s2[100];
+	printf("Enter the first string: ");
+	scanf("%s",s1);
+	printf("Enter the second string: ");
+	scanf("%s",s2);
+	
+	concat(s1,s2);
+	printf("The concatenated string: %s",s1);
+	
+	return 0;
+}
--- Exam/exam2.cpp
+++ Exam/exam2.cpp
@@ -1,3 +1,35 @@
 #include <stdio.h>
+#include <string.h>
 
+int CountVowel(char str[],char ch, int count){
+	count = 0;
+	int i=0;
+	while(str[i++] != '\0'){
+		if(str[i] == ch ){
+			count++;
+		}
+	}
+	return count;
+}
 
+int main(){
+	char s1[100];
+	printf("Enter string: ");
+	scanf("%s",s1);
+	
+	int vowel_a, vowel_e, vowel_i, vowel_o, vowel_u;
+	
+	CountVowel(s1,'a',vowel_a);
+	CountVowel(s1,'e',vowel_e);
+	CountVowel(s1,'i',vowel_i);
+	CountVowel(s1,'o',vowel_o);
+	CountVowel(s1,'u',vowel_u);
+	
+	printf("Number of charactors: \n");
+	printf("a: %d; e: %d; i: %d, o: %d, u: %d\n",vowel_a,vowel_e,vowel_i,vowel_o,vowel_u);
+	
+	int total;
+	total = vowel_a + vowel_e + vowel_i + vowel_o + vowel_u;
+	printf("Percentages of total:\n");
+	printf("a: %f; e: %f; i: %f, o: %f, u: %f\n",(float)vowel_a/total,(float)vowel_e/total,(float)vowel_i/total,(float)vowel_o/total,(float)vowel_u/total);	
+}
